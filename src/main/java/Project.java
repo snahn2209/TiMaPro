b20b70c4c6c4a9e524180f3b7b162aa968faeade
PiaDue <pia.duenow@web.de>
2022-12-07 12:50:39 +0100

    added example JavaDoc

--- src/main/java/Project.java
+++ src/main/java/Project.java
@@ -1,10 +1,18 @@
+/**
+ * Class to store Projects
+ */
 public class Project {
+
     private String name;
 
     public Project(String name) {
         this.name = name;
     }
 
+    /**
+     * getter for project name
+     * @return name
+     */
     public String getName() {
         return name;
     }
