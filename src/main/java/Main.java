add901039988eb86d32f922544fff0abeacace1e
Sonsiri <106734463+SoniTiger@users.noreply.github.com>
2022-12-13 16:19:06 +0100

    implemented insertProject, selectProject and it's tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.SQLIntegrityConstraintViolationException;
 
 public class Main {
@@ -7,12 +8,17 @@ public class Main {
         UserDataProvider.insertUser("Pia");
 
         UserAccount user = UserDataProvider.selectUser("Pia");
-        System.out.println(user.toString());*/
+        System.out.println(user.toString());
         Connection con = DBConnection.getConnection();
 
         UserAccount user = UserDataProvider.insertUser(con, "Mayaaa");
         System.out.println(user.toString());
 
+        DBConnection.disconnect(con);*/
+
+        Connection con = DBConnection.getConnection();
+        Project project7 = ProjectDataProvider.insertProject(con, "Project7", Date.valueOf("2025-07-13"));
+        System.out.println(project7.toString());
         DBConnection.disconnect(con);
     }
 }
--- src/main/java/ProjectDataProvider.java
+++ src/main/java/ProjectDataProvider.java
@@ -1,4 +1,38 @@
+import java.sql.*;
+
 public class ProjectDataProvider {
     //TODO: insert Project
+    public static Project insertProject(Connection con, String name, Date deadline) throws SQLIntegrityConstraintViolationException {
+
+        if (con != null) {
+            try {
+                DBConnection.insert(con, "INSERT INTO projects(name,deadline) VALUES('" + name + "','"+ deadline +"')");
+                return ProjectDataProvider.selectProject(con, name);
+            } catch (SQLIntegrityConstraintViolationException e) {
+                throw e;
+            }
+        }
+        return null;
+    }
+
     //TODO: select Project
+    public static Project selectProject(Connection con, String name) {
+        if (con != null) {
+            ResultSet rs = DBConnection.select(con, "SELECT * FROM projects WHERE name='" + name + "'");
+            try {
+                if (rs != null) {
+                    while (rs.next()) {
+                        return new Project(
+                                rs.getInt("projectID"),
+                                rs.getString("name"),
+                                rs.getDate("deadline")
+                        );
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
 }
