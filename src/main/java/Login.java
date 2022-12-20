6d462982774f34fa58bb0763b37ceaecb82006e2
Sonsiri <106734463+SoniTiger@users.noreply.github.com>
2022-12-20 17:22:19 +0100

    implemented basics for Frontend Login

---
+++ src/main/java/Login.java
@@ -0,0 +1,8 @@
+import static spark . Spark .*;
+
+public class Login {
+
+    /*public static void main(String[] args) {
+        get("/", (req, res) -> {return "hello <b>HTW</b>";} );
+    }*/
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,14 @@
+import spark.ModelAndView;
+
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static spark . Spark .*;
+import spark.template.jade.JadeTemplateEngine;
 
 public class Main {
     public static void main(String[] args) throws SQLIntegrityConstraintViolationException {
@@ -21,5 +28,15 @@ public class Main {
         Project project7 = ProjectDataProvider.insertProject(con, "Project7", Date.valueOf("2025-07-13"));
         System.out.println(project7.toString());
         DBConnection.disconnect(con);*/
-    }
-}
+
+
+        //http://localhost:4567/
+        get("/", (req, res) -> {return "<b>Welcome to TMProject</b>";} );
+        //http://localhost:4567/TMProject
+        get("/TMProject", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            ModelAndView modelAndView = new ModelAndView(model, "TMProject");
+            return modelAndView;
+        }, new JadeTemplateEngine());
+
+    }}
